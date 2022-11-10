a0562844b3c17a8545156138426004232d080bba
Kamal Nayan <geniusamansingh@gmail.com>
2022-11-10 14:15:25 +0000

    adding moralis ipfs filecoin intialised

--- millow/src/components/Navigation.js
+++ millow/src/components/Navigation.js
@@ -12,7 +12,6 @@ const Navigation = ({ account, setAccount }) => {
         <nav>
             <ul className='nav__links'>
                 <li><a href="#">Buy</a></li>
-                <li><a href="#">Rent</a></li>
                 <li><a href="#">Sell</a></li>
             </ul>
 
